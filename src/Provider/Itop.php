76b3d4299c8f25f9c67a51b96cfc1c40250544ac
odain <olivier.dain@combodo.com>
2025-04-03 08:55:59 +0200

    adapt Itop provider to handle oauth2 without consent form as well

--- src/Provider/Itop.php
+++ src/Provider/Itop.php
@@ -18,15 +18,16 @@ use Hybridauth\User\Profile;
  * Itop Oauth2 Connect provider adapter.
  *
  * Example:
- *         'Keycloak' => [
+ *         'Itop' => [
  *             'enabled' => true,
  *             'url' => 'your-itop-url', // depending on your setup you might need to add '/auth'
  *             'realm' => 'your-realm',
- *              'environnement' => 'your-environnement', //usually production
+ *              'environnement' => 'your-environnement', //default is production
  *             'keys' => [
  *                 'id' => 'client-id',
  *                 'secret' => 'client-secret'
- *             ]
+ *             ],
+ * 				'consent' => true, //default is consent mode enabled
  *         ]
  *
  */
@@ -45,6 +46,7 @@ class Itop extends OAuth2
 	private string $environnement = "production";
 	protected $tokenExchangeMethod = 'POST';
 	protected string $authentTokenBaseUrl;
+	protected bool $consentRequired = true;
 
 	/**
 	 * {@inheritdoc}
@@ -73,11 +75,23 @@ class Itop extends OAuth2
 			}
 		}
 
+        if ($this->config->exists('consent')) {
+            $this->consentRequired = $this->config->get('consent');
+        }
+
 		$this->apiBaseUrl = $url;
 
-		$this->authentTokenBaseUrl = sprintf("%s/env-%s/%s/", $url, $this->environnement, "authent-token");
+
+		$this->authentTokenBaseUrl = sprintf('%s/pages/exec.php?exec_module=authent-token&exec_env=%s&exec_page=', $url, $this->environnement);
 		$this->authorizeUrl = $this->authentTokenBaseUrl."authorize.php";
 		$this->accessTokenUrl = $this->authentTokenBaseUrl.'token.php';
+	}
+
+    protected function getAuthorizeUrl($parameters = [])
+    {
+        $url = parent::getAuthorizeUrl($parameters);
+        $iPos = strrpos($url, '?');
+        return substr_replace($url, '&', $iPos, 1);
 	}
 
 	/**
@@ -86,6 +100,10 @@ class Itop extends OAuth2
 	protected function initialize()
 	{
 		parent::initialize();
+
+        $this->AuthorizeUrlParameters += [
+            'prompt' => $this->consentRequired ? 'consent' : 'noconsent'
+        ];
 
 		$this->tokenExchangeParameters = [
 			'client_id' => $this->clientId,
@@ -108,6 +126,58 @@ class Itop extends OAuth2
 		$this->apiRequestParameters = [
 			'version' => $this->version,
 		];
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+    public function authenticate()
+    {
+        if ($this->consentRequired){
+            return parent::authenticate();
+        }
+
+        $this->logger->info(sprintf('%s::authenticate()', get_class($this)));
+
+        if ($this->isConnected()) {
+            return true;
+        }
+
+        try {
+            $authUrl = $this->getAuthorizeUrl();
+            $this->logger->debug(sprintf('%s::authenticateBegin(), redirecting user to:', get_class($this)), [$authUrl]);
+
+            $response = $this->httpClient->request(
+                $authUrl,
+                'POST',
+                $this->AuthorizeUrlParameters,
+                $this->tokenExchangeHeaders
+            );
+            $this->validateApiResponse('Unable to reach authorize endpoint in no consent form mode');
+            $data = (new \Hybridauth\Data\Parser())->parse($response);
+
+            $collection = new \Hybridauth\Data\Collection($data);
+
+            if (!$collection->exists('code')) {
+                throw new InvalidAccessTokenException(
+                    'Provider returned no code: ' . htmlentities($response)
+                );
+            }
+
+            $response = $this->exchangeCodeForAccessToken($collection->get('code'));
+
+            var_dump($response);
+            $this->validateAccessTokenExchange($response);
+
+            $this->initialize();
+
+        } catch (Exception $e) {
+            $this->clearStoredData();
+
+            throw $e;
+        }
+
+        return null;
 	}
 
 	/**
